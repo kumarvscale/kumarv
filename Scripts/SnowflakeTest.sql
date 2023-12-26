f00037f16e704d40b87bfc1b44703f786fb8ac5d
kumarvscale <114450796+kumarvscale@users.noreply.github.com>
2023-12-26 10:01:30 -0800

    Seal test v3

--- Scripts/SnowflakeTest.sql
+++ Scripts/SnowflakeTest.sql
@@ -1,9 +1,5 @@
---scale_playpen.goldfinch_holdings.trade_data 
-
-    -- FILEPATH: /Users/vishalkumar/Documents/Git-Repo/kumarv/Scripts/SnowflakeTest.sql
-    -- BEGIN: abpxx6d04wxr
-    SELECT security_instrument_identifier, AVG(trade_price_amount) as avg_trade_price
-    FROM scale_playpen.goldfinch_holdings.trade_data 
-    WHERE security_instrument_identifier = '013817AW1' AND b_as_of_date = (SELECT max(b_as_of_date) FROM scale_playpen.goldfinch_holdings.trade_data ) AND trade_date >= DATEADD('week', -52, CURRENT_DATE())
-    GROUP BY 1
-    -- END: abpxx6d04wxr
+Select params:templateVariables:Category::string as Category,
+params:templateVariables:Domain::string as Domain,
+params:templateVariables:Guidance::string as Guidance,
+count(_id) as Tasks_Count
+ from tasks where project='6578e5ee0b5783697f4d57f2' and status!='canceled' group by all
